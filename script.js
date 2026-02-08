022d8a6b93e2a2f861801fb32db07dcfbe12c115
dj-pmfst <djuric@pmfst.hr>
2026-02-08 18:15:46 +0100

    developing choice handling

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ function newGame(){
         };
         rounds.push(round);
 
-        fetch('YOUR_API_URL/rounds', {
+        fetch('https://restful-api.dev/rounds', {
             method: 'POST',
             headers: {
               'Content-Type': 'application/json'
@@ -31,8 +31,69 @@ function newGame(){
           .then(data => console.log('Round created:', data))
           .catch(error => console.error('Error:', error));
     };  
+    document.getElementsByClassName('start').disabled = false;
 };
+
+document.getElementsByClassName('new__game').addEventListener('click', newGame());
 
 function start(){
+    document.getElementsByClassName(start).disabled = false;
+    document.getElementsByClassName('new__game').disabled = true;
 
+    currentRound = 1;
+    gameState = states[1];
+
+    fetch(`https://restful-api.dev/rounds?gameId=${currentGameId}&roundNumber=1`, {
+      method: 'GET'}).then(response => response.json()).then(data => {
+          let currentRoundData = data;
+          //dodt korisnik display 
+      })
 };
+
+function handleChoices(move){
+    const roundId = rounds[currentRound - 1].id; 
+    const computerMove = rounds[currentRound - 1].computerMove;
+    const result = null;
+
+    fetch(`https://restful-api.dev/rounds/${roundId}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        playerMove: playerChoice
+      })
+    })
+    .then(response => response.json())
+    .then(data => {
+      console.log('Round updated:', data);
+    })
+    .catch(error => {
+      console.error('Error updating round:', error);
+    });
+
+    if (move === computerMove) {
+      result = "Draw";
+    }
+    
+    if ((move === 'rock' && computerMove === 'scissors') || (move === 'scissors' && computerMove === 'paper') || (move === 'paper' && computerMove === 'rock')) 
+    {
+      result = "Victory";
+      rounds[currentRound - 1].playerScore += 1;
+    } 
+    else {
+      result = "Defeat";
+    }
+}
+
+document.getElementById('rock-button').addEventListener('click', function() {
+  handleChoices('rock');
+});
+
+document.getElementById('paper-button').addEventListener('click', function() {
+  handleChoices('paper');
+});
+
+document.getElementById('scissors-button').addEventListener('click', function() {
+  handleChoices('scissors');
+});
